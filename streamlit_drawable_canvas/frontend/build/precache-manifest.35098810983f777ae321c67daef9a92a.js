02baaab93301cf2ab1033833766240c32c8b4e89
Gitjimr <166082003+Gitjimr@users.noreply.github.com>
2025-11-17 10:47:33 -0300

    Add files via upload

---
+++ streamlit_drawable_canvas/frontend/build/precache-manifest.35098810983f777ae321c67daef9a92a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ae84fdca4037b40d806196ceea7445",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d2feb2a8ff443339a4c",
+    "url": "./static/css/main.b112735b.chunk.css"
+  },
+  {
+    "revision": "a7d8ab19cd58f154f77e",
+    "url": "./static/js/2.8fb634e8.chunk.js"
+  },
+  {
+    "revision": "349298fbb30f981c1395376b153ee9a6",
+    "url": "./static/js/2.8fb634e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d2feb2a8ff443339a4c",
+    "url": "./static/js/main.14723405.chunk.js"
+  },
+  {
+    "revision": "77e61c67eb4f95e15891",
+    "url": "./static/js/runtime-main.ff285346.js"
+  }
+]);
